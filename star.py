f05cdb7c74293b0101b881d5b9fb91b590d3c5c9
Marin Pop <pmarin08@yahoo.com>
2022-11-02 22:44:40 +0200

    new

---
+++ star.py
@@ -0,0 +1,14 @@
+import turtle as z
+
+z.speed(0)
+z.pencolor('pink', )
+z.bgcolor('black')
+for a in range(150):
+    z.rt(a)
+    z.circle(81,a)
+    # z.circle(55,a)
+    z.fd(a)
+    z.rt(43)
+    # z.rt(60)
+    # z.rt(55)
+z.done()

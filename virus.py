71a4b2cea79ed98dfdadff657e6ed86d8dec1ce8
marin <pmarin08@yahoo.com>
2022-11-02 22:07:10 +0200

    new branch

---
+++ virus.py
@@ -0,0 +1,13 @@
+# from multiprocessing.connection import wait
+# from turtle import *
+import turtle as a
+a.speed(600)
+a.color('cyan')
+a.bgcolor('black')
+b=200;
+while(b>0):
+    a.left(b)
+    a.forward(b*1)
+    b=b-1
+a.done()
+
